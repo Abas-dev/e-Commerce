9dc51348cbaec5547576bfda38b0f05d4f36c41f
Abas-dev <abasisaac22@gmail.com>
2023-07-03 16:56:28 +0100

    added my first template

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+#in this application, i will be using classbase views for majority or all of the views 
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
