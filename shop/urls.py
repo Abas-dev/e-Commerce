a3bdf1ab58b7d26e932673a2dce5aee74d37e99b
Abas-dev <abasisaac22@gmail.com>
2023-07-04 13:29:10 +0100

    worked on the templates

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('',views.HomePageView.as_view(),name='homePage'),
-
+    path('about/',views.AboutUsPageView.as_view(),name='aboutPage'),
+    path('products/',views.ProductPageView.as_view(),name='product'),
+    path('products/<int:id>/',views.SingleProductPageView.as_view(),name='singleProduct')
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,19 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class AboutUsPageView(TemplateView):
+    template_name = 'about.html'    
+
+class ContactUsPageView(TemplateView):
+    template_name = 'contact.html'
+
+class ProductPageView(TemplateView):
+    template_name = 'product.html'
+
+class SingleProductPageView(TemplateView):
+    template_name = 'singleProduct.html'
+    
+       
+
+    
